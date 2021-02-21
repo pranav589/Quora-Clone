2909dd744bd9c6efde865f9f1f1ad55f46d53c7e
Pranav Bakale <pranav.bakale@gmail.com>
2021-02-21 19:34:16 +0530

    icon added

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,13 @@ import firebase from 'firebase'
 
 
 const firebaseConfig = {
-  //configs
+  apiKey: "AIzaSyCgvDFnHEe6vjKuuLzK0CqjZa4fq18C1K8",
+  authDomain: "quora-527b9.firebaseapp.com",
+  projectId: "quora-527b9",
+  storageBucket: "quora-527b9.appspot.com",
+  messagingSenderId: "12088768893",
+  appId: "1:12088768893:web:d9a0999bba8d9e19933414",
+  measurementId: "G-0H3JTJD8YZ"
   };
 
 
